1049adbfa72b0fa5e061bc8b89c86b9c130531b4
2kilobyte <qyeasinar@gmail.com>
2025-01-07 18:15:56 +0600

    fixing stuff

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise';
+import './envConfig'
 
 // Create a connection to the database
 const pool = mysql.createPool({
-    host: '34.126.218.98', // Replace with your MySQL host
-    user: 'schoolm', // Replace with your MySQL username
-    password: 'P9-y)?9R?0%q', // Replace with your MySQL password
-    database: 'school_management', // Replace with your database name
+    host: process.env.DB_HOST, // Replace with your MySQL host
+    user: process.env.DB_USER, // Replace with your MySQL username
+    password: process.env.DB_PASS, // Replace with your MySQL password
+    database: process.env.DB_NAME, // Replace with your database name
     waitForConnections: true
   });
   
