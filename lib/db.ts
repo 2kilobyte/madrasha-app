7794b4686578886ec72b8075646594ee665af759
2kilobyte <qyeasinar@gmail.com>
2025-01-07 17:21:08 +0600

    db fixed

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 // Create a connection to the database
 const pool = mysql.createPool({
-    host: 'localhost', // Replace with your MySQL host
-    user: 'selaikor_scmanagement', // Replace with your MySQL username
+    host: '34.126.218.98', // Replace with your MySQL host
+    user: 'schoolm', // Replace with your MySQL username
     password: 'P9-y)?9R?0%q', // Replace with your MySQL password
-    database: 'selaikor_sc_management', // Replace with your database name
+    database: 'school_management', // Replace with your database name
     waitForConnections: true
   });
   
